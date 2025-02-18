b1cb9b8dd1f376e0c47c8b6f460181d92e8a87f8
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2025-02-18 10:48:03 -0800

    Feat: add inital files

---
+++ app.js
@@ -0,0 +1,19 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages}, ${!this.title? "read": "not read yet"}`;
+  };
+}
+
+theHobbit = new Book("The Hobbit","J.R.R. Tolkien",295,false);
+console.log(theHobbit.info());
+
+let animal = {
+  eats: true
+};
+let rabbit = {
+  jumps: true
+};
