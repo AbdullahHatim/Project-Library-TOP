28e641e997ceb27663d4268902acb1f0d670cc89
Abdullah Hatim <182906341+AbdullahHatim@users.noreply.github.com>
2025-02-18 11:48:47 -0800

    Feat: Add book displaying functionality

--- app.js
+++ app.js
@@ -11,3 +11,34 @@ function addBookToLibrary(name, author, pages) {
   const book = new Book(name, author, pages);
   myLibrary.push(book);
 }
+
+function displayBooks(){
+  myLibrary.forEach((book)=>{
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    
+    const Name = document.createElement("h2");
+    Name.textContent = book.name;
+
+    const Author = document.createElement("p");
+    Author.textContent = `by ${book.author}`;
+
+    const PageCount = document.createElement("p");
+    PageCount.textContent = `${book.pages} Pages`;
+
+    bookCard.appendChild(Name);
+    bookCard.appendChild(Author);
+    bookCard.appendChild(PageCount);
+
+    const bookContainer = document.querySelector(".book-container");
+    const addButton = document.querySelector(".add-button");
+    bookContainer.insertBefore(bookCard,addButton);
+    
+  });
+}
+
+addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281);
+addBookToLibrary("1984", "George Orwell", 328);
+addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180);
+
+displayBooks();
